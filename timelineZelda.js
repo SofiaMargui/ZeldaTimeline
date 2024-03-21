435482bbf3c6c8a9373c82d2cce877d4f9e388a9
Robert Muresan <robotzelu89@gmail.com>
2024-03-21 20:34:25 +0000

    introducir el formulario

--- timelineZelda.js
+++ timelineZelda.js
@@ -1,29 +1,49 @@
 "use strict"
 
-//Obtener la info de la URL
-fetch(`https://gist.githubusercontent.com/bertez/8e62741154903c35edb3bfb825a7f052/raw/b5cd5137fd168116cc71740f1fbb75819d0fa82e/zelda-timeline.json`)
-    .then(resp => resp.json())
-    .then (data => {
+const fetchEvents = async () => {
+    const response = await fetch(`https://gist.githubusercontent.com/bertez/8e62741154903c35edb3bfb825a7f052/raw/b5cd5137fd168116cc71740f1fbb75819d0fa82e/zelda-timeline.json`);
 
-        //Ordenar los eventos por el año
-        const sortedEvents=data.sort((a, b) => a.date - b.date);
-        //console.log(sortedEvents)
+    if(response.ok) {
+        return await response.json();
+    }else{
+        throw new Error("No se pudieron obtener los datos");
+    }
+};
 
-        sortedEvents.forEach(event => {
-            const timeline = document.getElementById(`timeline`);
+const showEvents = (events) => {
+    const sortedEvents = events.sort((a, b) => a.date -b.date);
 
-            const eventElement = document.createElement(`div`);
-            eventElement.classList.add(`event`);
-            eventElement.innerHTML = `
-            
-            <div class="ev-date" > ${event.date}</div>
+    const timeline= document.querySelector(`#div-timeline`);
+    timeline.innerHTML = "" ;
+
+    sortedEvents.forEach((event) => {
+        const timeline = document.querySelector(`#div-timeline`);
+        const eventElement = document.createElement(`div`);
+        eventElement.classList.add(`event`);
+        eventElement.innerHTML = `
             <div class="event-info">
-                <h2 class="class-h2">${event.title}</h2>
-                <img class="class-image" src="${event.image}" alt ="Esto es una imagen de ${event.title}">
+                <h2 class="class-h2">${event.date}</h2>
+                <h3 class="class-h3">${event.title}</h3>
                 <p>${event.text}</p>
+                <img class="class-image" src="${event.image}" alt ="Esto es una imagen de ${event.title}">
             </div>    
-            `
-            timeline.appendChild(eventElement);
-        })
-    })
-    .catch(error => console.error(`Error al obtener el JSON:`, error));
+            `;
+
+        timeline.appendChild(eventElement);       
+        
+        
+    });
+};
+
+fetchEvents().then((events) => showEvents(events));
+
+const form = document.getElementById("addGame");
+form.onsubmit = async (event) => {
+    event.preventDefault();
+    const formData = Object.fromEntries(new FormData(event.target).entries());
+    event.target.reset();
+
+    const events = await fetchEvents();
+    events.push({...formData, date: parseInt(formData.date)});
+    showEvents(events);
+};
